7586a46fa19d183538ed9a485bac73a669668887
Gill-Singh-A <amansg22@iitk.ac.in>
2023-06-19 14:45:12 +0530

    Implemented Feature to get the commit count on the Default Branch of a repository of the User

--- main.py
+++ main.py
@@ -206,6 +206,14 @@ class Github:
         repo_html = BeautifulSoup(repo_page.content, "html.parser")
         star_tag = repo_html.find_all("a", attrs={"href": f"/{self.id}/{repo}/watchers"})[-1]
         return int(star_tag.text.strip().split('\n')[0])
+    def getRepoCommitCount(self, repo):
+        repo_link = f"{self.home_page_link}/{repo}"
+        repo_page = requests.get(repo_link)
+        if repo_page.status_code != 200:
+            return None
+        repo_html = BeautifulSoup(repo_page.content, "html.parser")
+        star_tag = repo_html.find_all("a", attrs={"href": f"/{self.id}/{repo}/commits/{self.getRepoDefaultBranch(repo)}"})[-1]
+        return int(star_tag.text.strip().split('\n')[0])
 
 if __name__ == "__main__":
     pass
