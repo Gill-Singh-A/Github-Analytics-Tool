e0d49425e4f07adc80da46e8b323b0ed88ec43c5
Gill-Singh-A <amansg22@iitk.ac.in>
2023-06-21 20:46:00 +0530

    Implemented inputs from CLI arguments and displaying some information

--- main.py
+++ main.py
@@ -2,8 +2,30 @@
 
 import requests, os
 from pathlib import Path
+from datetime import date
 from subprocess import run
+from optparse import OptionParser
 from bs4 import BeautifulSoup
+from pickle import dump, load
+from colorama import Fore, Back, Style
+from time import strftime, localtime, sleep, time
+
+status_color = {
+    '+': Fore.GREEN,
+    '-': Fore.RED,
+    '*': Fore.YELLOW,
+    ':': Fore.CYAN,
+    ' ': Fore.WHITE
+}
+
+def display(status, data, start='', end='\n'):
+    print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
+
+def get_arguments(*args):
+    parser = OptionParser()
+    for arg in args:
+        parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
+    return parser.parse_args()[0]
 
 class Github:
     github = "https://github.com/"
@@ -26,7 +48,7 @@ class Github:
     def getRepoCount(self):
         counter = self.home_page_html.find("span", attrs={"class": "Counter"})
         return int(counter.text)
-    def getRepos(self):
+    def getRepos(self, verbose=False):
         repos = []
         while len(repos) != self.repoCount:
             repo_page_link = f"{self.home_page_link}/{Github.repoTab}&{Github.page}{len(repos)//30+1}"
@@ -43,7 +65,30 @@ class Github:
                               "star_users": self.getRepoStarUsers(repository.get_attribute_list(key="href")[0].split('/')[-1]),
                               "watchers": self.getRepoWatchers(repository.get_attribute_list(key="href")[0].split('/')[-1]),
                               "forks": self.getRepoForkCount(repository.get_attribute_list(key="href")[0].split('/')[-1]),
-                              "topics": self.getRepoTopics(repository.get_attribute_list(key="href")[0].split('/')[-1])})
+                              "topics": self.getRepoTopics(repository.get_attribute_list(key="href")[0].split('/')[-1]),
+                              "languages": self.getRepoLanguages(repository.get_attribute_list(key="href")[0].split('/')[-1])})
+                repo = repos[-1]
+                if verbose:
+                    print()
+                    display(' ', f"Repository     = {Back.MAGENTA}{repo['name']}{Back.RESET}")
+                    display(':', f"Link           = {Back.MAGENTA}{repo['link']}{Back.RESET}")
+                    if repo['about'] != None:
+                        display(':', f"About          = {Back.MAGENTA}{repo['about']}{Back.RESET}")
+                    display(':', f"Default Branch = {Back.MAGENTA}{repo['default_branch']}{Back.RESET}")
+                    display(':', f"Commits        = {Back.MAGENTA}{repo['commits']}{Back.RESET}")
+                    display(':', f"Stars          = {Back.MAGENTA}{len(repo['star_users'])}{Back.RESET}")
+                    display(':', f"Watchers       = {Back.MAGENTA}{len(repo['watchers'])}{Back.RESET}")
+                    display(':', f"Forks          = {Back.MAGENTA}{repo['forks']}{Back.RESET}")
+                    if len(repo['languages']) != 0:
+                        display('+', "Languages")
+                        for langauge in repo["languages"]:
+                            display(':', f"\t* {langauge['percentage']} => {Back.MAGENTA}{langauge['name']}{Back.RESET}")
+                    if len(repo['topics']) != 0:
+                        display('+', "Topics")
+                        for topic in repo["topics"]:
+                            display(':', f"\t* {topic['name']} ({Back.MAGENTA}{topic['link']}{Back.RESET})")
+        if verbose:
+            print()
         return repos
     def getFollowers(self):
         page_number, followers = 1, []
@@ -111,11 +156,11 @@ class Github:
         nickname = vcard_names_tag.find("span", attrs={"itemprop": "additionalName"})
         pronouns = vcard_names_tag.find("span", attrs={"itemprop": "pronouns"})
         if name != None:
-            name_info["name"] = name.text.strip()
+            name_info["Name"] = name.text.strip()
         if nickname != None:
-            name_info["nickname"] = nickname.text.strip()
+            name_info["Nickname"] = nickname.text.strip()
         if pronouns != None:
-            name_info["pronouns"] = pronouns.text.strip()
+            name_info["Pronouns"] = pronouns.text.strip()
         return name_info
     def getWorkPlace(self):
         work_tag = self.home_page_html.find("li", attrs={"itemprop": "worksFor"})
@@ -284,7 +329,7 @@ class Github:
                     return about_tag.text.strip()
                 except AttributeError:
                     return None
-    def getLanguages(self, repo):
+    def getRepoLanguages(self, repo):
         languages = []
         repo_link = f"{self.home_page_link}/{repo}"
         repo_page = requests.get(repo_link)
@@ -296,6 +341,8 @@ class Github:
             if h2_tag.text == "Languages":
                 languange_tags = list(h2_tag.parents)[0].find_all("a")
                 break
+        else:
+            return []
         for language_tag in languange_tags:
             language = language_tag.text.strip().split('\n')
             languages.append({"name": language[0], "percentage": float(language[1][:-1])})
@@ -331,5 +378,96 @@ class Github:
         os.chdir(cwd)
 
 if __name__ == "__main__":
-    user = Github("Gill-Singh-A")
-    print(user.getRepos())
+    data = get_arguments(('-u', "--users", "users", "ID of the Users to get Details. (seperated by ',')"),
+                         ('-l', "--load", "load", "File from which to load the Users"),
+                         ('-w', "--write", "write", "Name of the file to dump extracted data"),
+                         ('-r', "--read", "read", "Read a dump file"),
+                         ('-a', "--account", "account", "Account of a User to login to, for getting more Details"))
+    if data.read:
+        exit(0)
+    if not data.users:
+        if not data.load:
+            display('-', f"Please Enter the {Back.MAGENTA}ID{Back.RESET} of the Users")
+        else:
+            try:
+                with open(data.load, 'r') as file:
+                    data.users = [user for user in file.read().split('\n') if user != '']
+            except FileNotFoundError:
+                display('-', f"File {Back.MAGENTA}{data.load}{Back.RESET} not found!")
+                exit(0)
+    else:
+        data.users = data.users.split(',')
+    users_data = {}
+    for user in data.users:
+        users_data[user] = {}
+        github_user = Github(user)
+        if github_user.home_page.status_code != 200:
+            display('-', f"{Back.MAGENTA}{user}{Back.RESET} not found")
+            continue
+        users_data[user]["names"] = github_user.VCardNames()
+        for name_type, name in users_data[user]["names"].items():
+            display(':', f"{name_type}:\t{Back.MAGENTA}{name}{Back.RESET}")
+        users_data[user]["pro"] = github_user.isPro()
+        if users_data[user]["pro"]:
+            display(':', f"User has a {Back.MAGENTA}PRO{Back.RESET} Account")
+        users_data[user]["mail"] = github_user.getMail()
+        if users_data[user]["mail"] != None:
+            display(':', f"Mail = {Back.MAGENTA}{users_data[user]['mail']}{Back.RESET}")
+        users_data[user]["location"] = github_user.getLocation()
+        if users_data[user]["location"] != None:
+            display(':', f"Location = {Back.MAGENTA}{users_data[user]['location']}{Back.RESET}")
+        users_data[user]["timezone"] = github_user.getLocalTimeZone()
+        if users_data[user]["timezone"] != None:
+            display(':', f"Time Zone = {Back.MAGENTA}{users_data[user]['timezone']}{Back.RESET}")
+        users_data[user]["organization"] = github_user.getOrganization()
+        if len(users_data[user]["organization"]) != 0:
+            display(':', f"Organization = {Back.MAGENTA}{users_data[user]['organization']}{Back.RESET}")
+        users_data[user]["workplace"] = github_user.getWorkPlace()
+        if users_data[user]["workplace"] != None:
+            display(':', f"Workplace = {Back.MAGENTA}{users_data[user]['workplace']}{Back.RESET}")
+        users_data[user]["links"] = github_user.getLinks()
+        if len(users_data[user]["links"]) != 0:
+            display('+', "Links")
+            for link in users_data[user]["links"]:
+                display(':', f"\t{link['name']}:\t{Back.MAGENTA}{link['link']}{Back.RESET}")
+        users_data[user]["status"] = github_user.getStatus()
+        if users_data[user]["status"] != None:
+            display(':', f"Status = {Back.MAGENTA}{users_data[user]['status']}{Back.RESET}")
+        users_data[user]["bio"] = github_user.getBio()
+        if users_data[user]["bio"] != None and users_data[user]["bio"] != '':
+            display(':', f"Bio: {users_data[user]['bio']}")
+        users_data[user]["achievements"] = github_user.getAchievements()
+        if len(users_data[user]["achievements"]) != 0:
+            display('+', f"Achievements")
+            for achievement in users_data[user]["achievements"]:
+                display(':', f"\t{Back.MAGENTA}{achievement['name']}{Back.RESET}: {achievement['count']}")
+        users_data[user]["followers"] = github_user.getFollowers()
+        if len(users_data[user]["followers"]) != 0:
+            display('+', "Followers")
+            for follower in users_data[user]["followers"]:
+                display(':', f"\t* {follower} ({Back.MAGENTA}{Github.github}{follower}{Back.RESET})")
+        users_data[user]["following"] = github_user.getFollowing()
+        if len(users_data[user]["following"]) != 0:
+            display('+', "Following")
+            for following in users_data[user]["following"]:
+                display(':', f"\t* {following} ({Back.MAGENTA}{Github.github}{following}{Back.RESET})")
+        display(':', "Fetching Repository Data...")
+        users_data[user]["repos"] = github_user.getRepos(verbose=True)
+        display('+', "Gathered Repository Data")
+        display(':', f"Total Repositories = {Back.MAGENTA}{len(users_data[user]['repos'])}{Back.RESET}")
+        users_data[user]["starred_repos"] = github_user.getStarRepos()
+        if len(users_data[user]["starred_repos"]) != 0:
+            display('+', "Starred Repositories")
+            for starred_repo in users_data[user]["starred_repos"]:
+                display(':', f"\t* {starred_repo['name']} ({Back.MAGENTA}{starred_repo['link']}{Back.RESET})")
+        users_data[user]["contribution_calendar"] = github_user.getContributionCalendar()
+        working_days = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0}
+        contributions = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0}
+    if data.write:
+        cwd = Path.cwd()
+        data_folder = cwd / "data"
+        data_folder.mkdir(exist_ok=True)
+        display(':', f"Dumping Data in File = {str(data_folder)}/{data.write}")
+        with open(f"data/{data.write}", 'wb') as file:
+            dump(users_data, file)
+        display('+', f"Dumped Data in File = {str(data_folder)}/{data.write}")
